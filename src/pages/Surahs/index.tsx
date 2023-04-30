dc19444017c57cfa88399570354a33b0c4582813
husanbek123 <husanbek555@gmail.com>
2023-04-30 15:09:59 +0500

    321 commmit

--- src/pages/Surahs/index.tsx
+++ src/pages/Surahs/index.tsx
@@ -4,26 +4,31 @@ import GetData1 from "../../api/Query";
 import useStore from "../../Store";
 import Surah from "../../components/surah";
 import AudioPlayer from "../../components/audioPlayer";
+import Loading from "../../components/loading";
 
 type Props = {};
 
 let Surahs: React.FC<Props> = ({}: Props) => {
   let { currentAuthor, currentSurah } = useStore((state) => state);
-  let { data } = GetData1(
+  let { data, isLoading } = GetData1(
     ["quran"],
     `https://api.alquran.cloud/v1/quran/${currentAuthor}`
   );
 
-  return (
-    <>
-      {currentSurah !== 0 && <AudioPlayer />}
-      <br />
-      <div className={styles.surahs}>
-        {data?.data?.surahs?.map((surah: Interfaces.Surah) => (
-          <Surah {...surah} />
-        ))}
-      </div>
-    </>
-  );
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    return (
+      <>
+        {currentSurah !== 0 && <AudioPlayer />}
+        <br />
+        <div className={styles.surahs}>
+          {data?.data?.surahs?.map((surah: Interfaces.Surah) => (
+            <Surah {...surah} />
+          ))}
+        </div>
+      </>
+    );
+  }
 };
 export default Surahs;
